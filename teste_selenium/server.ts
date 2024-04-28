c917c49187f3e8b6416b2356145c4b281801a75e
Roberta Nunes da Rocha Brandao <roberta@robertabrandao.com.br>
2024-04-28 14:25:08 -0300

    save jobs

--- teste_selenium/server.ts
+++ teste_selenium/server.ts
@@ -1,30 +1,48 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
 import 'chromedriver';
 
+
+// Configurar o driver do Selenium para o navegador Chrome
+// var driver = await new Builder().forBrowser('chrome').build();
+
 // Configurar o WebDriver para usar o Selenium Grid,
 // disponivel no docker-composer no repositorio
+
 var driver = await new Builder()
   .usingServer('http://localhost:4444/wd/hub') // URL do hub do Selenium Grid
   .forBrowser('chrome')
   .build();
 
+//variavem do site
 var urlTeste = "https://robertabrandao.com.br";
 
 async function executarTeste(seleniumDriver: object) {
-  // Configurar o driver do Selenium para o navegador Chrome
-  // let driver = await new Builder().forBrowser('chrome').build();
   
   let driver = seleniumDriver;
 
   try {
-    // Abrir o site robertabrandao.com.br
+    // 1. Abrir o site robertabrandao.com.br
     await driver.get(urlTeste);
 
-    // Esperar até que uma determinada condição seja atendida (por exemplo, a página carregar)
+    // 2. Esperar até que uma determinada condição seja atendida (por exemplo, a página carregar)
     await driver.wait(until.titleIs('Roberta Brandão'), 10000);
     
-    // Encontrar um elemento na página (por exemplo, o caminho relativo)
-    validarLinkPorTexto('Texto do Link', 'pagina-de-destino');
+    // 3. Encontrar um elemento na página (por exemplo, o camino de menu)
+    
+      //validar menus:
+      validarLinkPorTexto('Posts', 'posts');
+      validarLinkPorTexto('Posts', 'posts');
+      validarLinkPorTexto('Posts', 'posts');
+
+    // 4. Verificar se o título da postagem está correto.
+    // 5. Verificar se o conteúdo da postagem está sendo exibido corretamente.
+    // 6. Envio de comentários em postagens.
+    // 7. Navegação entre páginas.
+    // 8. Visualização de postagens.
+    // 9. Validação de links e botões.
+    // 10. Responsividade do site em diferentes dispositivos.
+
+
 
     // Exemplo de saída de informações
     console.log('Teste executado com sucesso!');
@@ -75,4 +93,4 @@ async function validarLinkPorTexto(textoDoLink: string, urlEsperada: string, sel
 
 
 // Chamar a função para executar o teste
-runTest();
+executarTeste(driver);
